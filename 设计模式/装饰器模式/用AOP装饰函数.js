38b8ef32232160ff3750ec2508c1f832a23fba09
imxlss <imxlss@163.com>
2019-08-29 14:59:17 +0800

    装饰器模式

--- 设计模式/装饰器模式/用AOP装饰函数.js
+++ 设计模式/装饰器模式/用AOP装饰函数.js
@@ -1,19 +1,29 @@
-Function.prototype.before = function(beforefn) {
+Function.prototype.before = function (beforefn) {
   let _self = this;
 
-  return function() {
+  return function () {
     beforefn.apply(this, arguments);
 
     return _self.apply(this, arguments);
   };
 };
 
-Function.prototype.after = function(afterfn) {
+Function.prototype.after = function (afterfn) {
   let _self = this;
 
-  return function() {
+  return function () {
     let ret = _self.apply(this, arguments);
     afterfn.apply(this, arguments);
     return ret;
   };
 };
+
+
+/* 不污染原型 */
+
+let before = function (fn, beforefn) {
+  return function () {
+    beforefn.apply(this, arguments);
+    return fn.apply(this, arguments);
+  }
+}
