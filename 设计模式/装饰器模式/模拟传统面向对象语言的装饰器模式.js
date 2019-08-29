81de0455a8c9a904fb607d21367677d8bb9bd950
imxlss <imxlss@163.com>
2019-08-29 08:42:38 +0800

    add 装饰器模式

---
+++ 设计模式/装饰器模式/模拟传统面向对象语言的装饰器模式.js
@@ -0,0 +1,28 @@
+let Plane = function() {};
+
+Plane.prototype.fire = function() {
+  console.log('发射普通子弹');
+};
+
+let MissileDecorator = function(plane) {
+  this.plane = plane;
+};
+
+MissileDecorator.prototype.fire = function() {
+  this.plane.fire();
+  console.log('发射导弹');
+};
+
+let AtomDecorator = function(plane) {
+  this.plane = plane;
+};
+
+AtomDecorator.prototype.fire = function() {
+  this.plane.fire();
+  console.log('发射原子弹');
+};
+
+let plane = new Plane();
+plane = new MissileDecorator(plane);
+plane = new AtomDecorator(plane);
+plane.fire();
